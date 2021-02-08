3796a6e6005e6cb79ab7826566ffeab5787cb2b0
Tucker <gillitux@gmail.com>
2021-02-07 22:47:58 -0500

    updated readme and endpoint to communicate with deployed database

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 const config = {
-  API_ENDPOINT: 'http://localhost:8000/api',
+  API_ENDPOINT: 'https://morning-fortress-77195.herokuapp.com/api',
   //   API_KEY: process.env.REACT_APP_API_KEY,
 };
 
